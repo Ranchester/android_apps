71a5bcbde18a7039f542c06c29ced1832ea68252
Ranchester <akreivi@gmail.com>
2013-11-07 13:01:51 +0200

    splash screen add

--- gen/com/pack/basics/R.java
+++ gen/com/pack/basics/R.java
@@ -56,6 +56,7 @@ public final class R {
         public static final int example2=0x7f030002;
         public static final int example3=0x7f030003;
         public static final int example4=0x7f030004;
+        public static final int main=0x7f030005;
     }
     public static final class menu {
         public static final int main_menu=0x7f060000;
---
+++ src/com/pack/basics/Splash.java
@@ -0,0 +1,38 @@
+package com.pack.basics;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		Thread startTimer = new Thread(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					sleep(1000);
+					Intent i = new Intent("com.pack.basics.MAINACTIVITY");
+					startActivity(i);
+					
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					
+				}
+				
+				finally {
+					finish();
+				}
+			}
+		};
+		startTimer.start();
+	}
+
+}
